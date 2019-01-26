2a76342d1b0ceb4b39c41310fe397dd3096ee741
Dylan Hahn <dylan@localhost.localdomain>
2019-01-25 23:42:07 -0500

    Base scheduler functionality

--- obligation.gd
+++ obligation.gd
@@ -1,8 +1,13 @@
-var startTime = 0
-var endTime = 0
-
-# params: Obligation obligation
-func equals(obligation):
-	if startTime == obligation.startTime && endTime == obligation.endTime:
-		return true
-	return false
+class obligation:
+	var startTime = 0
+	var endTime = 0
+	
+	func _init(start, end):
+		startTime = start
+		endTime = end
+	
+	# params: Obligation obligation
+	func equals(obligation):
+		if startTime == obligation.startTime && endTime == obligation.endTime:
+			return true
+		return false
