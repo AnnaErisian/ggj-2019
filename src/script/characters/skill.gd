7972fe3601cf71d57c67756b1ae2d818b2f260a0
Jack Thornton <jwthornt@ncsu.edu>
2019-01-26 01:27:01 -0500

    Added party, character and skill classes. Tested character class, need to test others.

--- src/script/characters/skill.gd
+++ src/script/characters/skill.gd
@@ -1,6 +1,15 @@
-var name = ""
-var description = ""
-
-var xp = 0
-var level = 0
-
+class skill:
+	const xp_to_level = 100
+	
+	var name = ""
+	var description = ""
+	
+	var xp = 0
+	
+	func _init(skillName, skillDescription, baseXp):
+		name = skillName
+		description = skillDescription
+		xp = baseXp
+	
+	func level():
+		return xp / 100
