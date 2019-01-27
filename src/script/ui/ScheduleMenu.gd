c1e60801ee70ed263321c17fd4c26754e9175acf
Dylan Hahn <dylan@localhost.localdomain>
2019-01-26 22:54:14 -0500

    Schedule menu progress

--- src/script/ui/ScheduleMenu.gd
+++ src/script/ui/ScheduleMenu.gd
@@ -5,10 +5,14 @@ const ScheduleMenuItem = preload("res://src/scene/ui/Schedule.tscn")
 var party
 
 func loadScheduleMenu():
+	for child in get_node("ColorRect/ScrollContainer/Schedules").get_children():
+		get_node("ColorRect/ScrollContainer/Schedules").remove_child(child)
+		
 	party = get_tree().get_root().get_node("Main").get_node("MainData").party
 	
+	var count = 0
 	for character in party.active:
 		var scheduleItem = ScheduleMenuItem.instance()
 		scheduleItem.loadCharacterData(character)
+		scheduleItem.rect_min_size = Vector2(2000, 100)
 		get_node("ColorRect/ScrollContainer/Schedules").add_child(scheduleItem)
-		
